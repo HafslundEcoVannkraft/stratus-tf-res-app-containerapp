f7ebd1ae3be41c9a9bda7cfb2c72ae3945b44b73
Hein Tonny Køien <hein.tonny.koien@hafslund.no>
2025-06-03 01:33:58 +0200

    Fix issue with locals for dns zone names

--- dns.tf
+++ dns.tf
@@ -1,7 +1,7 @@
 # Create CNAME for the Container App pointing to the managed environment
 resource "azurerm_private_dns_cname_record" "private" {
   name                = local.app_config.name
-  zone_name           = local.private_dns_zone.name
+  zone_name           = local.private_dns_zone_name
   resource_group_name = "${var.code_name}-dns-zones-rg-${var.environment}"
   ttl                 = 300
 
@@ -11,7 +11,7 @@ resource "azurerm_private_dns_cname_record" "private" {
 # Create CNAME for the Container App pointing to the public CNAME for the container app environment
 resource "azurerm_private_dns_cname_record" "public" {
   name                = local.app_config.name
-  zone_name           = local.public_dns_zone.name
+  zone_name           = local.public_dns_zone_name
   resource_group_name = "${var.code_name}-dns-zones-rg-${var.environment}"
   ttl                 = 300
 
