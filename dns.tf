d0843b1020bc65ea1a0cb8dce67bf05ce7aab04f
Hein Tonny Køien <hein.tonny.koien@hafslund.no>
2025-06-03 02:28:47 +0200

    Fix issue with dns cname record is string

--- dns.tf
+++ dns.tf
@@ -1,19 +1,9 @@
 # Create CNAME for the Container App pointing to the managed environment
-resource "azurerm_private_dns_cname_record" "private" {
+resource "azurerm_private_dns_cname_record" "app" {
   name                = local.app_config.name
   zone_name           = local.private_dns_zone_name
   resource_group_name = "${var.code_name}-dns-zones-rg-${var.environment}"
   ttl                 = 300
 
-  record = ["${local.container_app_environment_name}.${local.private_dns_zone_name}"]
-}
-
-# Create CNAME for the Container App pointing to the public CNAME for the container app environment
-resource "azurerm_private_dns_cname_record" "public" {
-  name                = local.app_config.name
-  zone_name           = local.public_dns_zone_name
-  resource_group_name = "${var.code_name}-dns-zones-rg-${var.environment}"
-  ttl                 = 300
-
-  record = ["appgw01.stratus.hafslund.no"]
+  record = "${local.container_app_environment_name}.${local.private_dns_zone_name}"
 }
